223aaed788c5809723113cf66ffe2864874ef481
WAYLANbruintjies <waylan.bruintjies@icloud.com>
2023-02-16 23:33:10 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This program prints a string or line with the help of the puts function
+ *Return = 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+return = 0
+}	
