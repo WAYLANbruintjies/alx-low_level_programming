91ccbea9d1dd922a69ef16d641f42747c9d67456
WAYLANbruintjies <waylan.bruintjies@icloud.com>
2023-02-17 03:16:52 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,9 @@
  * main - A program that prints a string with printf function
  * Return: 0 (Success)
  */
+
 int main(void)
 {
-	printf("\"with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("\"with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
