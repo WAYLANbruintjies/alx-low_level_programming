b03e7ad236dd434152dbf5b587db275ed1300f09
WAYLANbruintjies <waylan.bruintjies@icloud.com>
2023-02-18 03:43:03 -0800

    The alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the alphabet in lowercase using putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int putchar("a .. z\n");
+	return (0);
+}
