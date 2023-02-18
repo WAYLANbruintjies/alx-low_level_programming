2d2c2c9afafb2c593335af52ad68028e748c37f3
WAYLANbruintjies <waylan.bruintjies@icloud.com>
2023-02-18 05:58:07 -0800

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,10 +7,9 @@
 int main(void)
 {
 	char alp;
-	char alp2;
 	for (alp = 'a'; alp <= 'z'; alp++)
 	{
-	for (alp2 = 'A'; alp <= 'Z'; alp++)
+	for (alp = 'A'; alp <= 'Z'; alp++)
 	{
 		putchar(alp);
 	}
