03072ecc3ab4b9c9ed864b86e4a481c7dd2200ac
WAYLANbruintjies <waylan.bruintjies@icloud.com>
2023-02-18 04:00:03 -0800

    The alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,6 +5,12 @@
  */
 int main(void)
 {
-	int putchar("a .. z\n");
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i <= 26; i++)
+	{	putchar(alp[i]);
+	}
+	putchar("\n");
 	return (0);
 }
