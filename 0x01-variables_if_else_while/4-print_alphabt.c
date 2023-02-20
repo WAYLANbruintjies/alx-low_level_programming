769bbfcead6d851a04457f5130b16424fbeb37a1
WAYLANbruintjies <waylan.bruintjies@icloud.com>
2023-02-20 01:00:29 -0800

    Alphabet soup

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,17 +7,20 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-
 {
 	char low, e, q;
+
 	e = 'e';
 	q = 'q';
 
 	for (low = 'a'; low <= 'z'; low++)
 	{
 	if (low != e && != q)
+
 	putchar(low);
 	}
+
 	putchar('\n');
+
 	return (0);
 }
