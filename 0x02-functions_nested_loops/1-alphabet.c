d16da040363e6d9df168e6147f4d84a86a32d8a8
WAYLANbruintjies <waylan.bruintjies@icloud.com>
2023-02-22 02:24:21 -0800

    The alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - A function to print the alphabet in lowercase, new line follows
+ */
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		_putchar(alpha);
+		_putchar ('\n');
+}
