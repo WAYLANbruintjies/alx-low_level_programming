50e7e04d77fe8c1b28c939820f40d8c3704e8fc1
WAYLANbruintjies <waylan.bruintjies@icloud.com>
2023-02-26 13:17:29 -0800

    File: 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - Write a function that checks for uppercase character
+ * @c : Is the character to be checked
+ * Return: 1 if c is uppercase or 0 if not
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 99)
+	{
+	return (1);
+	}
+	return (0);
+}
