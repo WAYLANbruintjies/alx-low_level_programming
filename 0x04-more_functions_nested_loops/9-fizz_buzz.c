a436506bd629f56c27f6c12afd34559802e5849c
WAYLANbruintjies <waylan.bruintjies@icloud.com>
2023-02-27 03:23:43 -0800

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints the numbers from 1 to 100
+ * For multiples of three print Fizz
+ * For multiples of five print Buzz
+ * For multiples of both three and five and five, print FizzBuzz
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0 && n % 5 != 0)
+		{
+			printf("Fizz");
+		}
+		else if (n % 5 == 0 && n % 3 != 0)
+		{
+			printf("Buzz");
+		}
+		else if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (n == 1)
+		{
+			printf("%d", n);
+		}
+		else
+		{
+			printf("%d", n);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
