0f8f77e6f18ba0e2bd3d528b2d8bd3c8718f88b9
WAYLANbruintjies <waylan.bruintjies@icloud.com>
2023-02-27 00:54:51 -0800

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,16 @@
 
 /**
  * print_numbers - Print the numbers from 0 to 9
- * Return: Numbers from 0 to 9
+ * Return: void
  */
 
 void print_numbers(void)
 {
-	int n;
+	char c;
 
-	for (n = 0; n <= 9; n++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(n + '0');
+		_putchar(c);
 	}
 	_putchar('\n');
 }
