35eac42293b003a958698ff6caaa6668404ac97f
WAYLANbruintjies <waylan.bruintjies@icloud.com>
2023-03-02 14:56:19 -0800

    Concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,22 +10,18 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int a;
-	int b;
+	int dlen = 0, i;
 
-	a = 0;
-	while (dest[a] != '\0')
+	while (dest[dlen])
 	{
-		a++;
+		dlen++;
 	}
-	b = 0;	
-	while (src[b] != '\0')
+	for (i = 0; src[i] != '\0'; i++)
 	{
-		dest[a] = src[b];
-		a++;
-		b++;
+		dest[dlen] = src[i];
+		dlen++;
 	}
-	dest[a] = '\0';
+	dest[dlen] = '\0';
 
 	return (dest);
 }
