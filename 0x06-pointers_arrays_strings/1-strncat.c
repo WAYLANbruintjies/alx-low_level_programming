87018688e9fe429b98f35b83778984ae65e5b6f8
WAYLANbruintjies <waylan.bruintjies@icloud.com>
2023-03-02 15:15:34 -0800

    1. strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncat - Concatenates two strings
+ * @dest: Input string
+ * @src: Input string
+ * @n: Number of bytes
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+	{
+		dest_len++;
+	}
+	for (index = 0; src[index] && index < n; index++)
+	{
+		dest[dest_len] = src[index];
+	}
+	return (dest);
+}
