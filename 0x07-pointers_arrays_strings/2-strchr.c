f1956f63a01d7592f938272e78897c01a7f1a958
WAYLANbruintjies <waylan.bruintjies@icloud.com>
2023-03-06 09:59:47 -0800

    2. strchr mandatory Write a function that locates a character in a string.

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - Locates a character in a string
+ * @s: The string the be checked
+ * @c: Character to be identify
+ * Result: String s if c char is found, NULL if not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+		return (s);
+		}
+		s++;
+	}
+	if (*s == c)
+	{
+	return (s);
+	}
+	return (NULL);
+}
