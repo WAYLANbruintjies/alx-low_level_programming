2d98fc97085a5dcc8434f6f24ec75589e73086b9
WAYLANbruintjies <waylan.bruintjies@icloud.com>
2023-03-06 11:27:51 -0800

    5. strstr mandatory Write a function that locates a substring.

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strstr - Locates a substring
+ * @haystack: String to search
+ * @needle: Substring to search
+ * Return: Pointer to the beginning of the located substring, or NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+			{
+				break;
+			}
+		}
+		if (needle[j] == '\0')
+		{
+			return (&haystack[i]);
+		}
+	}
+	return (NULL);
+}
