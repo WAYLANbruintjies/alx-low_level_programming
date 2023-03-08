9825fce0caf4866ac080ee703b938cd4311a647b
WAYLANbruintjies <waylan.bruintjies@icloud.com>
2023-03-08 03:10:44 -0800

    A function that returns the length of a string.

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns the length of a string
+ * @s: The string to be computed
+ * Return: Integer value
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		s++;
+	return (_strlen_recursion(s) +1);
+	}
+}
