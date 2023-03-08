9b5ead395ad82ad85f771d78932c73cc24242dc7
WAYLANbruintjies <waylan.bruintjies@icloud.com>
2023-03-08 08:11:21 -0800

    A function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0.

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 int check_prime(int n, int i);
+
 /**
  * is_prime_number - Return 1 for prime num and 0 if not
  * @n: Input value
  * Return Integer value
  */
-
 int is_prime_number(int n)
 {
 	return (check_prime(n, 1));
 }
+
 /**
  * check_prime - Check for prime numbers
  * @n: Input value to be checked
