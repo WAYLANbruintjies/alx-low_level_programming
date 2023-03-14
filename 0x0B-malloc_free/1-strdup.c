1f1721f989dec1c7b205d2c15b64fc754d4d744f
WAYLANbruintjies <waylan.bruintjies@icloud.com>
2023-03-14 14:15:47 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strdup - Duplicates a string
+ * @str: The string to be duplicated
+ * Return: Duplicated string
+ */
+char *_strdup(char *str)
+{
+	int a = 0, i = 1;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[i])
+	{
+		i++;
+	}
+	s = malloc((sizeof(char) * 1) + 1);
+
+	if (s == NULL)
+		return (NULL);
+
+	while (a < i)
+	{
+		s[a] = str[a];
+		a++;
+	}
+	s[a] = '\0';
+	
+	return (s);
+}
