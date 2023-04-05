6bcf04f091ca0a3af7cdc4cc237fa745ee246d72
Waylan Bruintjies <waylandyzz632@gmail.com>
2023-04-05 14:15:31 -0700

    2. Add node (betty compliant)

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,10 +1,9 @@
 #include "lists.h"
 
-/*
+/**
  * add_nodeint - Adds a new node at the beginning of a list
  * @head: double pointer to list
  * @n: new data variable
- * @listint_t: the list to be computed
  * Return: the address of the new element, or NULL if it failed
  */
 listint_t *add_nodeint(listint_t **head, const int n)
