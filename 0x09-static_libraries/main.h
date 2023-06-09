611f4396a1b300d3161e30e84cba20a61dcbd8e5
WAYLANbruintjies <waylandyzz632@gmail.com>
2023-06-09 09:46:44 -0700

    main.h updated

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -24,3 +26,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif
