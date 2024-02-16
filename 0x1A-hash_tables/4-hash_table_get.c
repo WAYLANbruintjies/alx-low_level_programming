72cec65d58871b5e2bc10c596c3064595c51f99a
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-02-16 03:43:36 -0800

    Retrieves a value associated with a key

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,27 +1,27 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get - Retrieve a value associated with a key
+ * hash_table_get - Retrieve value associated with a key
  * @ht: pointer to hash table
- * @key: is the lookup element to get from hash table
- *
- * Return: value of item within hash table or NULL
+ *@key: is the lookup element to get from hash table
+ * 
+ * Return: the item in hash table or NULL.
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	hash_node_t *newnode;
-	unsigned long int idx;
+	hash_node_t *addnode;
+	unsigned long int index;
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
 
-	idx = key_index((const unsigned char *)key, ht->size);
-	if (idx >= ht->size)
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
 		return (NULL);
 
-	newnode = ht->array[idx];
-	while (newnode && strcmp(node->key, key) != 0)
-		newnode = newnode->next;
+	addnode = ht->array[index];
+	while (addnode && strcmp(addnode->key, key) != 0)
+		addnode = addnode->next;
 
-	return ((newnode == NULL) ? NULL : newnode->value);
+	return ((addnode == NULL) ? NULL : addnode->value);
 }
