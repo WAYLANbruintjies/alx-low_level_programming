f5ebda7559f54974005edb915e1faccfc7e7614b
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-02-16 03:50:03 -0800

    Retrieves a value associated with a key

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,19 +9,19 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	hash_node_t *addnode;
+	hash_node_t *node;
 	unsigned long int index;
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
 
-	index = key_index((const unsigned char *)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size;
 	if (index >= ht->size)
 		return (NULL);
 
-	addnode = ht->array[index];
-	while (addnode && strcmp(addnode->key, key) != 0)
-		addnode = addnode->next;
+	node = ht->array[index];
+	while (node && strcmp(node->key, key) != 0)
+		node = node->next;
 
-	return ((addnode == NULL) ? NULL : addnode->value);
+	return ((node == NULL) ? NULL : node->value);
 }
